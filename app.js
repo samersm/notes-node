65a3118c9ed7392f36f8c39d6dfa85007d2022e7
samer <mrsamer123@hotmail.com>
2017-08-25 10:40:01 +0000

    Added age require function

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-console.log('Starting app');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
 var user = os.userInfo();
 
-// console.log(user);
-
-// fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
-
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
