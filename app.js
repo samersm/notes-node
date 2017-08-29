ed27760f843c6a9806be6ffbce81a1cf0bcdb743
samer <mrsamer123@hotmail.com>
2017-08-29 19:14:11 +0000

    Added yargs package and learned about argv

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+
+const argv = yargs.argv;
+var command = argv._[0];
 console.log('Command: ', command);
-console.log(process.argv);
+// console.log('Process',process.argv);
+console.log('Yargs', argv);
+
 
 if (command === 'add') {
-    console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Fetching all notes');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removing all notes');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,9 +1,26 @@
 console.log('Starting notes.js');
 
-
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
-  
+var addNote = (title, body) => {
+  console.log("Adding note", title, body);
 };
 
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+//Challenge teacher answer
+var getNote = (title) => {
+  console.log('Getting notes', title);
+};
+
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
